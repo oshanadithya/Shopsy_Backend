9e169047af990b6964cfe194ce923a35c60409ee
o_weerasinghe <oshanweerasinghe28@gmail.com>
2022-03-23 12:51:02 +0530

    BackendReady

--- server.js
+++ server.js
@@ -33,9 +33,11 @@ const { connect } = require("mongodb");
 const complaintsRouter = require("./routes/ComplaintRoute");
 const contactus = require("./routes/ContactusRoute");
 
-app.use("/complaint", complaintsRouter );
-app.use("/contactus", contactus);
-
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number ${PORT}`);
 });
+
+
+
+app.use("/complaint", complaintsRouter );
+app.use("/contactus", contactus);

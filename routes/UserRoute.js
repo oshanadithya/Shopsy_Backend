875471f7502ad66944b09e8e0077a6a67c33e350
Chamuditha Weerasinghe <80471215+Chamudithawee@users.noreply.github.com>
2022-05-08 06:52:25 +0530

    update route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -45,7 +45,7 @@ router.route("/add").post((req, res) => {
       });
   });
 
-  //user email retrive route
+  //user email check route
 
   router.route("/check/:Email").get(async (req, res) => {
     const Email = req.params.Email;
@@ -74,6 +74,37 @@ router.route("/add").post((req, res) => {
       });
   });
   
-  
+  //getting one person Detail
+
+router.route("/get_one/:id").get(async(req,res)=>{s
+    const user = req.params.Email;
+    const itin = await user.findById(user).then(()=>{
+        res.status(200).send({status : "get Insurence Package Details" , data : itin});
+    }).catch((err)=>{
+        res.status(500).send({status : "getting unsuccesful!"});
+    });
+  });
+
+  //update Route
+router.route('/update-user/:Email').put(async (req, res) => {
+
+  const updates = req.body;
+
+  let Email = req.params.Email;
+
+  console.log(updates);
+
+  await user.findOneAndUpdate({Email}, updates)
+  .then(() => {
+    res.json("Updated Sucessfully!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+
+
+
   
   module.exports = router;
